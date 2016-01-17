d5fd887546c75b5450b8b9e8c0d7349371d815c7
Bin4ry <olabv@hotmail.com>
2016-01-17 21:41:15 +0100

    Removed test file

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$api_key = "D738E349D52B98C71497031B08ED70A2";
-$data    = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=D738E349D52B98C71497031B08ED70A2&steamids=76561197960435530');
-$decode  = json_decode($data, true);
-
-echo $decode['response']['players'][0]['personaname'];
-
-?>
